1b1314afbccaeb3ff21d0318e4f16cd431bdba6c
rendrap <rendrapradana@gmail.com>
2026-01-07 11:54:35 +0700

    setup

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,15 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={1}
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={(event) => {}}
 
       // How do I get autocomplete with JSX?
+
     />
   );
 };
+
+
+type Example = React.FormEventHandler<HTMLDivElement> | undefined;
