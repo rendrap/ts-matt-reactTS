8fc3bb59c71b84b36cb96e73c1dbc17cbe7bc267
rendrap <rendrapradana@gmail.com>
2026-01-11 16:36:27 +0700

    e-08

--- src/02-components/08-using-html-props.problem.tsx
+++ src/02-components/08-using-html-props.problem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Button = ({ className, ...rest }: {}) => {
+export const Button = ({ className, ...rest }: React.ComponentProps<"button">) => {
   return (
     <button {...rest} className={`default-classname ${className}`}></button>
   );
