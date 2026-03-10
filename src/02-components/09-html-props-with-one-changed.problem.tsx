0f7f825906bf1173df0364c499ab98284f7207e3
rendrap <rendrapradana@gmail.com>
2026-03-10 17:00:33 +0700

    e-09

--- src/02-components/09-html-props-with-one-changed.problem.tsx
+++ src/02-components/09-html-props-with-one-changed.problem.tsx
@@ -1,8 +1,12 @@
 import { ComponentProps } from "react";
 import { Equal, Expect } from "../helpers/type-utils";
 
+type InputProps = Omit<ComponentProps<"input">, "onChange"> & {
+  onChange: (value: string) => void;
+};
+
 export const Input = (
-  props: ComponentProps<"input"> & { onChange: (value: string) => void }
+  props: InputProps
 ) => {
   return (
     <input
